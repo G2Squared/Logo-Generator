78d6c500cec3e04f62970a051ba6f1be28dfae5c
G2Squared <gesket127@gmail.com>
2023-12-06 19:08:03 -0600

    Working

---
+++ index.js
@@ -0,0 +1,21 @@
+//To Import Classes
+const Shape = require 
+const Circle = require 
+const Square = require 
+const Triangle = require 
+
+const inquirer = require 
+const maxLengthInputPrompt = require 
+
+const fs = require 
+
+function generateSvg
+
+
+function createLogo
+
+const getLogoInfo = () => {
+    return inquirer.prompt ([
+        
+    ])
+}
